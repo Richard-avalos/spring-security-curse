26d17de86a918d52485c778d63fd81e8738703ce
Richard Avalos <ag0716012023@unab.edu.sv>
2026-05-03 11:38:01 -0600

    First Lesson

---
+++ src/main/java/com/raavalos/spring_security/controllers/AccountsController.java
@@ -0,0 +1,18 @@
+package com.raavalos.spring_security.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@RequestMapping(path= "/accounts")
+public class AccountsController {
+    @GetMapping
+    public Map<String, String> accounts() {
+        // Bussiness Logic
+        return Collections.singletonMap("msg", "Accounts");
+    }
+}
---
+++ src/main/java/com/raavalos/spring_security/controllers/CardsController.java
@@ -0,0 +1,18 @@
+package com.raavalos.spring_security.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@RequestMapping(path= "/Cards")
+public class CardsController {
+    @GetMapping
+    public Map<String, String> cards() {
+        // Bussiness Logic
+        return Collections.singletonMap("msg", "Cards");
+    }
+}
